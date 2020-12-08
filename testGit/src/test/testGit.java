593729c654458472de030ac57fa064d1e629bd89
mikinori0309 <mochimochimotimoti@gmail.com>
2020-12-08 14:28:24 +0900

    3行

--- testGit/src/test/testGit.java
+++ testGit/src/test/testGit.java
@@ -3,7 +3,8 @@ package test;
 public class testGit {
 
 	public static void main(String[] args) {
-
+		System.out.println("fuck you");
+		System.out.println("fuck you");
 		System.out.println("fuck you");
 	}
 
