d6a83fa6c18e1bc0471db5f89a6077caefa63915
mikinori0309 <mochimochimotimoti@gmail.com>
2020-12-09 09:44:28 +0900

    追記

--- testGit/src/test/testGit.java
+++ testGit/src/test/testGit.java
@@ -6,7 +6,7 @@ public class testGit {
 		System.out.println("fuck you");
 		System.out.println("fuck you");
 		System.out.println("fuck you");
-		System.out.println("接続成功");
+		System.out.println("接続成功165112");
 	}
 
 }
